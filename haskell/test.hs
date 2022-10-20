741463f32a260fbaaf1d8cb61377bd1e5a37f090
Josh <joshinbrackets@gmail.com>
2022-10-20 09:27:17 +0800

    head remake

--- haskell/test.hs
+++ haskell/test.hs
@@ -1,2 +1,4 @@
-main :: IO ()
-main = putStrLn "Hello Haskell"
+head' :: [a] -> a
+head' [] = error "No head for empty lists!!!"
+head' (x:_) = x
+main = putStrLn (show (head' [1,2,3,4,5,6,7,8,9,0]))
