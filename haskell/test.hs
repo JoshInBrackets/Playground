7badfc575b0e744a1cc747ba37b755d1ecab22b1
Josh <joshinbrackets@gmail.com>
2022-10-22 18:25:16 +0800

    update

--- haskell/test.hs
+++ haskell/test.hs
@@ -1,4 +1,4 @@
 head' :: [a] -> a
 head' [] = error "No head for empty lists!!!"
 head' (x:_) = x
-main = putStrLn (show (head' [1,2,3,4,5,6,7,8,9,0]))
+main = putStrLn (show (head' [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
