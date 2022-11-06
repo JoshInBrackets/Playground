3765d02df3db447ce8e3f3a680d0aac9f5572869
Josh <joshinbrackets@gmail.com>
2022-11-06 20:10:11 +0800

    more functions for caterpillar

--- python/caterpillar.py
+++ python/caterpillar.py
@@ -26,3 +26,40 @@ text_turtle.hideturtle()
 score_turtle = t.Turtle()
 score_turtle.hideturtle()
 score_turtle.speed(0)
+
+def outside_window():
+    pass
+
+def game_over():
+    pass
+
+def display_score(current_score):
+    pass
+
+def place_leaf():
+    pass
+
+def start_game():
+    global game_started
+    if game_started:
+        return
+    game_started = True
+    score = 0
+    text_turtle.clear()
+    caterpillar_speed = 2
+    caterpillar_length = 3
+    caterpillar.shapesize(1, caterpillar_length, 1)
+    display_score(score)
+    place_leaf()
+    while True:
+        caterpillar.forward(caterpillar_speed)
+        if caterpillar.distance(leaf) < 20:
+            place_leaf()
+            caterpillar_length += 1
+            caterpillar.shapesize(1, caterpillar_length, 1)
+            caterpillar_speed += 1
+            score += 10
+            display_score(score)
+        if outside_window():
+            game_over()
+            break
